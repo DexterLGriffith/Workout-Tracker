56ae7fbcd539c39b12ee8a27dfc80954aa4be8e9
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-12-18 15:57:36 -0600

    trying to get routes to run, theres an error somewhere

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,28 @@
 const router = require("express").Router();
+const { workout } = require("../model");
 
+
+router.get("/api/workouts", (req, res) => {
+    workout.find()
+    .then((result) => {
+        res.json(result)
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+router.put("/api/workouts/:id", (req, res) => {
+    const update = req.body; 
+    workout.findByIdAndUpdate(
+        req.params.id, 
+        {$push:{ exercises: update}},
+        {
+            new: true
+        })
+        .then((result) => {
+            res.json(result)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
